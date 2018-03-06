512c79410497658fee877e1d08987c67eed06fd7
salamanders <benjaminhill@gmail.com>
2018-03-06 14:47:35 -0800

    GPU works

--- src/main/kotlin/Learn.kt
+++ src/main/kotlin/Learn.kt
@@ -9,7 +9,6 @@ import org.deeplearning4j.arbiter.optimize.config.OptimizationConfiguration
 import org.deeplearning4j.arbiter.optimize.generator.RandomSearchGenerator
 import org.deeplearning4j.arbiter.optimize.parameter.continuous.ContinuousParameterSpace
 import org.deeplearning4j.arbiter.optimize.parameter.discrete.DiscreteParameterSpace
-import org.deeplearning4j.arbiter.optimize.parameter.integer.IntegerParameterSpace
 import org.deeplearning4j.arbiter.optimize.parameter.math.MathOp
 import org.deeplearning4j.arbiter.optimize.parameter.math.Op
 import org.deeplearning4j.arbiter.optimize.runner.LocalOptimizationRunner
@@ -25,6 +24,7 @@ import org.nd4j.linalg.activations.Activation
 import org.nd4j.linalg.api.buffer.DataBuffer
 import org.nd4j.linalg.api.buffer.util.DataTypeUtil
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator
+import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.lossfunctions.LossFunctions
 import java.io.File
 import java.util.concurrent.TimeUnit
@@ -32,7 +32,7 @@ import java.util.logging.Logger
 import kotlin.system.measureTimeMillis
 
 /**
- * Hello-world of deep learning using deeplearning4j, Kotlin, Gradle
+ * Hello-world of deep learning using deeplearning4j, Kotlin, Maven
  * Goals:
  *
  * <ol>
@@ -56,8 +56,8 @@ fun main(args: Array<String>) = measureTimeMillis {
     if (saveDir.exists()) saveDir.delete()
     saveDir.mkdir()
 
-    //println(Nd4j.getExecutioner().executionMode())
-    //println(Nd4j.getExecutioner().javaClass.simpleName)
+    println(Nd4j.getExecutioner().executionMode())
+    println(Nd4j.getExecutioner().javaClass.simpleName)
 
     // OPTIMIZATION, and only for GPU @see https://deeplearning4j.org/gpu
     DataTypeUtil.setDTypeForContext(DataBuffer.Type.HALF)
@@ -73,51 +73,51 @@ fun main(args: Array<String>) = measureTimeMillis {
     val rngSeed = 123456 // random number seed for reproducibility
     val numEpochs = 13 // complete training passes through the dataset.  15 initially, but lower for hyperparam search
     val activation = Activation.LEAKYRELU // Should be as-good-or-better than RELU @see https://arxiv.org/abs/1505.00853  reaches .98
+    val l2Scalar = 0.005 // magic (because it doesn't matter as much)
+    val outputActivation = Activation.SOFTMAX // Softmax sums to 1.  Hardmax has exactly one 1.
+    // "Avoids steep learning curve by picking weights from a Gaussian distribution based on number of input neurons"
+    val weightInit = WeightInit.XAVIER
+    val lossFunction = LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD // can be bad to optimize between
 
     // Optimizing hyperparameters
-    val learningRateHyperparam = ContinuousParameterSpace(0.0001, 0.1) // Are these sane bounds?
-    // Should be based on another param, @see https://github.com/deeplearning4j/Arbiter/issues/123
-    val l2Hyperparam = MathOp(learningRateHyperparam, Op.MUL, 0.005)
-    val layerSizeHyperparam = IntegerParameterSpace(outputNum, numRows * numColumns) // Is this necessary to optimize?
-    // Q: what is the difference?
-    val lossFunction = DiscreteParameterSpace(
-            LossFunctions.LossFunction.MCXENT,
-            LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD
-    )
-
+    val learningRateHyperparam = ContinuousParameterSpace(0.0001, 0.1)  // This large a range = ok.
+    // Unlikely to need much more than the avg(input, output)
+    val hiddenLayerSize = DiscreteParameterSpace(outputNum, 2 * outputNum, 4 * outputNum, 8 * outputNum)
 
     log.info("Config complete, building model....")
     val hyperparamSpace = MultiLayerSpace.Builder()
             .seed(rngSeed.toLong())
-            .trainingWorkspaceMode(WorkspaceMode.SEPARATE) // TODO enable when released
-            .weightInit(WeightInit.XAVIER) // "Avoids steep learning curve by picking weights from a Gaussian distribution based on number of input neurons"
-            .updater(AdamSpace(learningRateHyperparam)) // Why Adam?
-            .l2(l2Hyperparam)
+            .trainingWorkspaceMode(WorkspaceMode.SEPARATE)
+            .weightInit(weightInit)
+            .updater(AdamSpace(learningRateHyperparam)) // Adam usually beats raw gradient descent
+            .l2(MathOp(learningRateHyperparam, Op.MUL, l2Scalar))
             .addLayer(DenseLayerSpace.Builder() // input layer
                     .nIn(numRows * numColumns)
                     .activation(activation)
-                    .nOut(layerSizeHyperparam)
+                    .nOut(hiddenLayerSize)
                     .build())
             .addLayer(OutputLayerSpace.Builder() // hidden layer
-                    .activation(Activation.SOFTMAX) // Softmax sums to 1.  Hardmax has exactly one 1.
+                    .activation(outputActivation)
                     .lossFunction(lossFunction)
                     .nOut(outputNum)
                     .build())
             .build()
 
+    val candidateGenerator = RandomSearchGenerator(hyperparamSpace, null)
+    val dataProvider = object : DataProvider {
+        override fun getDataType(): Class<*> =
+                DataSetIterator::class.java
+
+        override fun trainData(dataParameters: MutableMap<String, Any>?): Any =
+                MultipleEpochsIterator(numEpochs, MnistDataSetIterator(batchSize, true, rngSeed))
+
+        override fun testData(dataParameters: MutableMap<String, Any>?): Any =
+                MnistDataSetIterator(batchSize, false, rngSeed)
+    }
 
     val configuration = OptimizationConfiguration.Builder()
-            .candidateGenerator(RandomSearchGenerator(hyperparamSpace, null))
-            .dataProvider(object : DataProvider {
-                override fun getDataType(): Class<*> =
-                        DataSetIterator::class.java
-
-                override fun trainData(dataParameters: MutableMap<String, Any>?): Any =
-                        MultipleEpochsIterator(numEpochs, MnistDataSetIterator(batchSize, true, rngSeed))
-
-                override fun testData(dataParameters: MutableMap<String, Any>?): Any =
-                        MnistDataSetIterator(batchSize, false, rngSeed)
-            })
+            .candidateGenerator(candidateGenerator)
+            .dataProvider(dataProvider)
             .modelSaver(FileModelSaver(saveDir.absolutePath))
             .scoreFunction(ScoreFunctions.testSetAccuracy())
             .terminationConditions(listOf(
